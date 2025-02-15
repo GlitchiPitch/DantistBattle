bbbf538f83259cef98faff7d534d7f47a26b1a77
GlitchiPitch <nikitareutsky@gmail.com>
2025-02-15 22:53:45 +0500

    upd player handler server

--- src/Shared/Systems/Round/Handlers/Player/Server/init.lua
+++ src/Shared/Systems/Round/Handlers/Player/Server/init.lua
@@ -13,14 +13,32 @@ local eventActions = require(event.Actions)
 local _connections: { RBXScriptConnection } = {}
 
 local function giveSkin(player: Player, role: Types.RoleType)
-    
+    local roleSkin = Instance.new("HumanoidDescription")
+    local humanoid = player.Character:FindFirstAncestorOfClass("Humanoid")
+    humanoid:ApplyDescription(roleSkin)
 end
 
 local function giveGun(player: Player, role: Types.RoleType)
-    
+    local roleGun = Instance.new("Tool")
+    roleGun:Clone().Parent = player.Backpack
+end
+
+local function setupStats(player: Player, role: Types.RoleType)
+    local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+    local roleStats = {
+        MaxHealth = 1000,
+        WalkSpeed = 20,
+        JumpPower = 100,
+    }
+
+    humanoid.MaxHealth = roleStats.MaxHealth
+    humanoid.Health = roleStats.MaxHealth
+    humanoid.WalkSpeed = roleStats.WalkSpeed
+    humanoid.JumpPower = roleStats.JumpPower
 end
 
 local function equipPlayer(player: Player, role: Types.RoleType)
+    setupStats(player, role)
     giveSkin(player, role)
     giveGun(player, role) 
 end
