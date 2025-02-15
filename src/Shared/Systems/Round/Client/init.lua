514b29c360bcd24c0bbe564b7d8cd0e3567ee521
GlitchiPitch <nikitareutsky@gmail.com>
2025-02-15 19:49:28 +0500

    init round system

---
+++ src/Shared/Systems/Round/Client/init.lua
@@ -0,0 +1,10 @@
+local RoundSystem = script.Parent
+local Events = RoundSystem.Events
+
+local function initialize()
+    
+end
+
+return {
+    initialize = initialize,
+}
---
+++ src/Shared/Systems/Round/Events/Remote/Actions.lua
@@ -0,0 +1,3 @@
+local Actions = {}
+
+return Actions
