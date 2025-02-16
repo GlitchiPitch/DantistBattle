23c15388cd10ec01922dc65af575a9788fcab3ae
GlitchiPitch <nikitareutsky@gmail.com>
2025-02-16 11:07:39 +0500

    initialize global timer to server & client

--- src/Server/Server/init.server.lua
+++ src/Server/Server/init.server.lua
@@ -1,7 +1,12 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Modules = {
+    GlobalTimer = ReplicatedStorage.GlobalTimer,
+}
+
 local function initialize()
-    
+    for _, module in Modules do
+        require(module).initialize()
+    end
 end
 
-return {
-    initialize = initialize,
-}
+initialize()
